414bdc48e0627761f7d1f929a9684c7b0402a910
Vonckad <Vonkad@yandex.ru>
2020-10-19 14:57:15 +0300

    Initial Commit

---
+++ WhiteStar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhiteStar
+//
+//  Created by Vlad Ralovich on 10/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
